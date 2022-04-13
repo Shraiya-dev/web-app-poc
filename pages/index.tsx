8a2b30ff5a014dc530ba20220773a2b2a97e470b
Lalit Tyagi <lalit.tyagi@remotestate.com>
2022-04-13 22:55:18 +0530

    dashboard done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,11 @@
 import { Button, MenuItem, Select, Stack, TextField } from '@mui/material'
 import { NextPage } from 'next'
 import { OnboardingLayout } from '../sdk'
-import DashbardLayout from '../sdk/layouts/DashbardLayout'
+import DashboardLayout from '../sdk/layouts/DashbardLayout'
 
 //*this is a test page to test out components for theming
 const Index: NextPage = () => {
-	// return <OnboardingLayout/>
-	return (<DashbardLayout/>)
+	return (<DashboardLayout/>)
 }
 
 export default Index
-
-//*testing the pages
-{
-	/* <Select
-				labelId='demo-simple-select-helper-label'
-				id='demo-simple-select-helper'
-				value={10}
-				label='Age'
-				sx={{
-					width: 500,
-				}}
-				MenuProps={{
-					sx: {
-						mt: 4,
-					},
-					PaperProps: {
-						elevation: 2,
-						sx: {
-							backgroundColor: '#efef3d',
-							maxHeight: 400,
-							mt: 1,
-							clipPath: 'polygon(0 8px, 20% 8px, 80% 8px, 90% 0, 100% 8px, 100% 100%, 0 100%)',
-							borderRadius: '4px ',
-						},
-					},
-					MenuListProps: {
-						sx: {
-							pt: 2,
-						},
-					},
-				}}> 
-				<MenuItem value=''>
-					<em>None</em>
-				</MenuItem>
-				<MenuItem value={10}>Ten</MenuItem>
-				<MenuItem value={20}>Twenty</MenuItem>
-				<MenuItem value={30}>Thirty</MenuItem>
-				<MenuItem value={20}>Twenty</MenuItem>
-				<MenuItem value={30}>Thirty</MenuItem>
-			</Select>
-			*/
-}
--- sdk/layouts/DashbardLayout.tsx
+++ sdk/layouts/DashbardLayout.tsx
@@ -1,59 +1,56 @@
-import styled from '@emotion/styled'
-import { AppBar, Box, Button, Chip, Grid, Paper, Stack, Toolbar, Typography } from '@mui/material'
+import { AppBar, Box, Button, Chip, Grid, Paper, Stack, Toolbar, Typography, Container, styled } from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
 import logo from '../../public/assets/icons/BrandLogo.svg'
 
 
+
+
 const CustomizeDashboard = styled(Box)(({ theme }) => ({
-    minHeight: "calc(100vh - 100px)",
-    backgroundColor:"yellow",
-    '.AppBar':{
-        backgroundColor:"gray",
-        maxHeight:"100px",
+    minHeight: 'calc(100vh - 100px)',
+    // backgroundColor: "yellow",
+    '.AppBar': {
+        backgroundColor: '#FFFFFF',
+        maxHeight: '100px',
     },
-    '.AppStack':{
-        padding: "25px 40px"
+    '.AppStack': {
+        padding: '25px 80px',
     },
-    '.headerBox':{
-        marginTop:"100px",
-        // padding:"20px 40px", 
-        border:"2px solid red"
+    '.headerBox': {
+        marginTop: '100px',
     },
+    
 }))
 
-const DashbardLayout = ({ children, ...props }: any) => {
+const DashboardLayout = ({ children, ...props }: any) => {
     return (
         <CustomizeDashboard>
             <AppBar className='AppBar' elevation={0}>
-                <Toolbar>
-                    <Stack width={"100%"} p={4} direction="row" className='AppStack' >
-                        <Box>
-                            <Image src={logo} alt="" height={45} />
-                        </Box>
-                        <Stack width={"30%"} direction="row" justifyContent="space-evenly" ml={"auto"}>
-                            <Button variant="text" sx={{fontWeight:800 , fontSize: "14px" }} >Dashboard</Button>
-                            <Button variant="text" sx={{ fontWeight: 800, fontSize: "14px" }} >My Profile</Button>
-                            <Button variant="contained" sx={{ fontWeight: 800, fontSize: "14px" }} >Book Worker</Button>
+                <Container>
+                    <Toolbar sx={{
+                        justifyContent: 'space-between'
+                    }}>
+                        <Image src={logo} alt='' height={52} width={162} />
+                        <Stack direction='row' spacing={2} >
+                            <Button variant='text' sx={{ fontWeight: 800, fontSize: '14px' }}>
+                                Dashboard
+                            </Button>
+                            <Button variant='text' sx={{ fontWeight: 800, fontSize: '14px' }}>
+                                My Profile
+                            </Button>
+                            <Button variant='contained' sx={{ fontWeight: 800, fontSize: '14px' }}>
+                                Book Worker
+                            </Button>
                         </Stack>
-                    </Stack>
-                </Toolbar>
+                    </Toolbar>
+                </Container>
+
             </AppBar>
-           <Box className='headerBox' >
-                <Grid container rowSpacing={2} columnSpacing={8} >
-                    <Grid item xs={12} md={4} >
-                        <Paper elevation={3} sx={{ backgroundColor: "red" }}>hi</Paper>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <Paper elevation={3}>hi</Paper>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                        <Paper elevation={3}>hi</Paper>
-                    </Grid>
-                </Grid>
-           </Box>
+            <Container className='headerBox'>
+                {children}
+            </Container>
         </CustomizeDashboard>
     )
 }
 
-export default DashbardLayout
+export default DashboardLayout
