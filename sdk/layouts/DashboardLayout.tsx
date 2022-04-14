85ee76c538842ea0f93a3da12c80602f65c36a58
Lalit Tyagi <lalit.tyagi@remotestate.com>
2022-04-14 20:14:18 +0530

    drawer-layout done

--- sdk/layouts/DashboardLayout.tsx
+++ sdk/layouts/DashboardLayout.tsx
@@ -1,7 +1,11 @@
-import { AppBar, Button, Container, Stack, styled, Toolbar } from '@mui/material'
+import { AppBar, Box, Button, Container, Divider, Drawer, IconButton, Menu, Stack, styled, Toolbar, Typography } from '@mui/material'
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../public/assets/icons/BrandLogo.svg'
+import { useMobile } from '../hooks/useMobile'
+import MenuIcon from '../../public/assets/icons/MenuIcon.svg'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
 
 //always update when you change the app bar height into the onlyCssWeNeed file
 const APP_BAR_HEIGHT = 84
@@ -27,15 +31,48 @@ const CustomContainer = styled(Container)(({ theme }) => ({
 }))
 
 const DashboardLayout = ({ children, ...props }: any) => {
+	const isMobile = useMobile()
+	const [state, setState] = useState<boolean>(false)
+	const toggle = () => {
+		setState(!state)
+	}
+
 	return (
 		<>
 			<CustomAppBar elevation={0}>
 				<Container className='container'>
 					<Toolbar className='toolbar'>
+						{isMobile ? (
+							<IconButton
+								key={'left'}
+								onClick={() => {
+									toggle()
+								}}>
+								<Image src={MenuIcon} alt="menu" color='black' />
+							</IconButton>
+
+						) : null}
+						<Drawer anchor={'left'} open={state} onClose={toggle}>
+							<Box width={250} mt={4}>
+								<Container>
+									<Image alt="logo" src={logo} />
+									<Button variant='text'> <DashboardIcon /> &nbsp; Dashboard</Button>
+									<Divider/>
+									<Button variant='text'> <PersonIcon /> &nbsp; My Profile</Button>
+								</Container>
+								
+								
+							</Box>
+						</Drawer>
 						<Image src={logo} alt='' height={52} width={162} />
 						<Stack direction='row' spacing={2}>
-							<Button variant='text'>Dashboard</Button>
-							<Button variant='text'>My Profile</Button>
+							{isMobile ? null : (
+								<>
+									<Button variant='text'>Dashboard</Button>
+									<Button variant='text'>My Profile</Button>
+								</>
+							)}
+
 							<Button variant='contained'>Book Worker</Button>
 						</Stack>
 					</Toolbar>
