9edeacbec61e0e8e8583c8659ea80276ed21361c
Lalit Tyagi <lalit.tyagi@remotestate.com>
2022-05-12 15:11:01 +0530

    pixels done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from '@mui/material/styles'
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
@@ -11,7 +11,6 @@ import {
 	SERVER_URL,
 	SnackbarProvider,
 	theme,
-	USER_LOGIN_TYPE,
 	USER_TYPE,
 } from '../sdk'
 import '../sdk/styles/onlyCssWeNeed.css'
@@ -89,6 +88,43 @@ function MyApp({ Component, pageProps }: AppProps) {
 		<>
 			<Head>
 				<title></title>
+				{/* Facebook Pixel Code  */}
+				{/* eslint-disable-next-line @next/next/next-script-for-ga */}
+				{process.env.NEXT_PUBLIC_SERVER_URL === 'https://api.projecthero.in' && (
+					<>
+						<script
+							dangerouslySetInnerHTML={{
+								__html: `
+						!(function (f, b, e, v, n, t, s) {
+							if (f.fbq) return
+							n = f.fbq = function () {
+								n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+							}
+							if (!f._fbq) f._fbq = n
+							n.push = n
+							n.loaded = !0
+							n.version = '2.0'
+							n.queue = []
+							t = b.createElement(e)
+							t.async = !0
+							t.src = v
+							s = b.getElementsByTagName(e)[0]
+							s.parentNode.insertBefore(t, s)
+						})(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+						fbq('init', '1285086655286614');
+						fbq('track', 'PageView');
+						`,
+							}}></script>
+						<noscript>
+							<img
+								height='1'
+								width='1'
+								src='https://www.facebook.com/tr?id=1285086655286614&ev=PageView&noscript=1'
+							/>
+						</noscript>
+					</>
+				)}
+				{/* End Facebook Pixel Code */}
 			</Head>
 			<ThemeProvider theme={theme}>
 				<SnackbarProvider>
