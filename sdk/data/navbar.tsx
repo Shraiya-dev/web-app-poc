02ab23f92bb69b1b2aa807bae73c5719d2ffe60a
Lalit Tyagi <lalit.tyagi@remotestate.com>
2022-07-23 14:30:43 +0530

    disabling faq page

--- sdk/data/navbar.tsx
+++ sdk/data/navbar.tsx
@@ -36,12 +36,12 @@ export const navbar: Navbar = {
 		// 	type: 'support_menu',
 		// 	label: 'Support',
 		// },
-		{
-			type: 'button_link',
-			label: 'FAQs',
-			icon: <HelpCenterRounded sx={{ fontSize: 36, color: 'primary.main' }} />,
-			link: '/faq',
-		},
+		// {
+		// 	type: 'button_link',
+		// 	label: 'FAQs',
+		// 	icon: <HelpCenterRounded sx={{ fontSize: 36, color: 'primary.main' }} />,
+		// 	link: '/faq',
+		// },
 		{
 			type: 'primary_button',
 			label: 'Book Worker',
