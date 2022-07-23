450396ea519fda7d6e54b7437ba008b9faea5a02
Lalit Tyagi <lalit.tyagi@remotestate.com>
2022-07-23 14:21:10 +0530

    hot fix removing access points for plans page

--- sdk/data/navbar.tsx
+++ sdk/data/navbar.tsx
@@ -20,12 +20,12 @@ export const navbar: Navbar = {
 			icon: <HomeRounded sx={{ fontSize: 36, color: 'primary.main' }} />,
 			link: '/',
 		},
-		{
-			type: 'button_link',
-			label: 'Plans',
-			icon: <CalendarMonth sx={{ fontSize: 36, color: 'primary.main' }} />,
-			link: '/hero/plans',
-		},
+		// {
+		// 	type: 'button_link',
+		// 	label: 'Plans',
+		// 	icon: <CalendarMonth sx={{ fontSize: 36, color: 'primary.main' }} />,
+		// 	link: '/hero/plans',
+		// },
 		{
 			type: 'button_link',
 			label: 'About Us',
