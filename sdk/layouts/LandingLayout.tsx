85b8d967ef52e220ac539ea13bde7b0a2aa938e4
Lalit Tyagi <lalit.tyagi@remotestate.com>
2022-08-19 01:30:35 +0530

    mergin domains

--- sdk/layouts/LandingLayout.tsx
+++ sdk/layouts/LandingLayout.tsx
@@ -1,22 +1,26 @@
-import { Stack } from '@mui/material'
+import { CssBaseline, Stack, ThemeProvider } from '@mui/material'
 import { Box } from '@mui/system'
 import { Footer, Navbar } from 'sdk/components'
+import { landingTheme } from 'sdk/constants/landingTheme'
 
 export const LandingLayout = ({ children }: any) => {
 	return (
 		<>
-			<Navbar />
+			<ThemeProvider theme={landingTheme}>
+				<CssBaseline />
+				<Navbar />
 
-			<Stack
-				mt='70px'
-				maxHeight='calc(100vh - 70px)'
-				minHeight='calc(100vh - 70px)'
-				sx={{
-					overflowY: 'scroll',
-				}}>
-				<Stack flex={1}>{children}</Stack>
-				<Footer />
-			</Stack>
+				<Stack
+					mt='70px'
+					maxHeight='calc(100vh - 70px)'
+					minHeight='calc(100vh - 70px)'
+					sx={{
+						overflowY: 'scroll',
+					}}>
+					<Stack flex={1}>{children}</Stack>
+					<Footer />
+				</Stack>
+			</ThemeProvider>
 		</>
 	)
 }
