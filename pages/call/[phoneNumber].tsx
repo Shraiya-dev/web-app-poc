55ca3321fde13dd110d5c44886f231b7193c5157
Lalit Tyagi <lalit.tyagi@remotestate.com>
2022-10-11 21:48:04 +0530

    fix done

--- pages/call/[phoneNumber].tsx
+++ pages/call/[phoneNumber].tsx
@@ -8,7 +8,7 @@ const Page: NextPage = () => {
 	const router = useRouter()
 	useEffect(() => {
 		if (typeof window !== 'undefined' && router.isReady) {
-			window.location.replace('tel:+919825252522')
+			window.location.replace('tel:+91' + router.query.phoneNumber)
 		}
 	}, [router])
 	return (
