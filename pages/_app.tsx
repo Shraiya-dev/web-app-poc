0b563281c0ff8c330bee5ee905e3d0e5e92f846b
Lalit Tyagi <lalit.tyagi@remotestate.com>
2023-01-03 16:57:49 +0530

    decricing default zoom

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -207,7 +207,7 @@ const CommonHead = () => {
 				<meta name='theme-color' content='#333333' />
 				<meta
 					name='viewport'
-					content='width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0'
+					content='width=device-width, initial-scale=0.7, maximum-scale=1.0,user-scalable=0'
 				/>
 				{process.env.NEXT_PUBLIC_APP_ENV === 'PROD' && (
 					<>
