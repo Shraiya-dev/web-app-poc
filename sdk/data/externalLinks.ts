e3139995cc19ab2ff6205c9fcc66669b7c72d5a4
Lalit Tyagi <lalit.tyagi@remotestate.com>
2023-02-03 14:00:33 +0530

    removing all old pages

--- sdk/data/externalLinks.ts
+++ sdk/data/externalLinks.ts
@@ -3,7 +3,7 @@ export const externalLinks = {
 		'&utm_source=website&utm_medium=website&utm_campaign=website_app_install&utm_content=app_install&utm_term=app_install',
 	heroPlayStoreApp: 'https://play.google.com/store/apps/details?id=com.projecthero.android&hl=en-IN',
 	contractorPlayStoreApp: 'https://play.google.com/store/apps/details?id=com.projecthero.contractor&hl=en-IN',
-	heroDeepLinkApp: 'https://projecthero.page.link/contowrk?hl=en-IN',
+	heroDeepLinkApp: 'https://projecthero.page.link/webtowrk?hl=en-IN',
 	contractorDeepLinkApp: 'https://projectherocontractor.page.link/wrktocon?hl=en-IN',
 	instaGram: 'https://www.instagram.com/projectheroworkers/?hl=en',
 	youtube: 'https://www.youtube.com/@ProjectHeroconstruction1021',
